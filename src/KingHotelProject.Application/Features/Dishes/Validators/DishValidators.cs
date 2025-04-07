93c04126207b01ee038f8b1fb4ebe9408c8bf234
shinsmathew <shinsmathew2537@gmail.com>
2025-04-07 18:40:17 +0200

    changes

--- src/KingHotelProject.Application/Features/Dishes/Validators/DishValidators.cs
+++ src/KingHotelProject.Application/Features/Dishes/Validators/DishValidators.cs
@@ -3,22 +3,6 @@ using KingHotelProject.Application.DTOs;
 
 namespace KingHotelProject.Application.Features.Dishes.Validators
 {
-    public class DishCreateValidator : AbstractValidator<DishCreateDto>
-    {
-        public DishCreateValidator()
-        {
-            RuleFor(x => x.DishName)
-                .NotEmpty().WithMessage("Dish name is required")
-                .MaximumLength(50).WithMessage("Dish name must not exceed 50 characters");
-
-            RuleFor(x => x.Price)
-                .NotEmpty().WithMessage("Price is required")
-                .GreaterThan(0).WithMessage("Price must be greater than 0");
-
-            RuleFor(x => x.HotelId)
-                .NotEmpty().WithMessage("Hotel ID is required");
-        }
-    }
 
     public class DishesBulkCreateValidator : AbstractValidator<DishesBulkCreateDto>
     {
@@ -28,9 +12,22 @@ namespace KingHotelProject.Application.Features.Dishes.Validators
                 .NotEmpty().WithMessage("At least one dish is required")
                 .Must(d => d.Count <= 100).WithMessage("Cannot create more than 100 dishes at once");
 
-            RuleForEach(x => x.Dishes).SetValidator(new DishCreateValidator());
+            RuleForEach(x => x.Dishes).ChildRules(dish =>
+            {
+                dish.RuleFor(x => x.DishName)
+                    .NotEmpty().WithMessage("Dish name is required")
+                    .MaximumLength(50).WithMessage("Dish name must not exceed 50 characters");
+
+                dish.RuleFor(x => x.Price)
+                    .NotEmpty().WithMessage("Price is required")
+                    .GreaterThan(0).WithMessage("Price must be greater than 0");
+
+                dish.RuleFor(x => x.HotelId)
+                    .NotEmpty().WithMessage("Hotel ID is required");
+            });
         }
     }
+
 
     public class DishUpdateValidator : AbstractValidator<DishUpdateDto>
     {
