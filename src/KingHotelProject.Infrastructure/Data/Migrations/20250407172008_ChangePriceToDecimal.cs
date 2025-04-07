d8b939ac61011ab5514b6472aaa5b792b26ebf45
shinsmathew <shinsmathew2537@gmail.com>
2025-04-07 19:22:31 +0200

    updates

---
+++ src/KingHotelProject.Infrastructure/Data/Migrations/20250407172008_ChangePriceToDecimal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KingHotelProject.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePriceToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
